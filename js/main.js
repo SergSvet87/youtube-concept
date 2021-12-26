ac6257cbf63b24078861f11e53bb62f86867af33
SergSvet87 <94105673+SergSvet87@users.noreply.github.com>
2021-12-26 15:10:46 +0200

    Пофиксил основной контент добавил каналы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Основной канал
 const channel = () => {
   let swiper = new Swiper('.channel-slider', {
     // Optional parameters
@@ -12,6 +13,7 @@ const channel = () => {
   })
 }
 
+// Канал рекомендации
 const recommended = () => {
   let swiper = new Swiper('.recommended-slider', {
     // Optional parameters
@@ -20,26 +22,28 @@ const recommended = () => {
 
     // Navigation arrows
     navigation: {
-      nextEl: '.channel-button-next',
-      prevEl: '.channel-button-prev',
+      nextEl: '.channel-recommended-button-next',
+      prevEl: '.channel-recommended-button-prev',
     },
   })
 }
 
+// Канал еда и напитки
 const food_drink = () => {
-  let swiper = new Swiper('.food_drink-slider', {
+  let swiper = new Swiper('.channel-food-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 6,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.channel-button-next',
-      prevEl: '.channel-button-prev',
+      nextEl: '.channel-food-button-next',
+      prevEl: '.channel-food-button-prev',
     },
   })
 }
 
+// Подключаем все каналы
 channel();
 recommended();
 food_drink();
